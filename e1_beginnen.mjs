c96f7260b675150e891a8a0dab20fbf9c7173462
CD-Port <cd.port.contact@gmail.com>
2024-10-14 13:06:30 +0200

    coner_depauw_e1_beginnen.mjs

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,7 @@
 // Voeg hier je eigen code in
 
-
+let name = 'Coner';
+let age = 29;
+let favoriteGame = 'World of Warcraft: Vanilla';
 
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
